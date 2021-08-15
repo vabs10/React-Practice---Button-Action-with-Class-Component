347c15ff5f9c1c272f1bbd97cdda8ae44f33012f
vabs10 <vyagneshwar98@gmail.com>
2021-08-15 19:56:11 +0530

    fixed problem

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,27 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
-	};
+
+		this.state = {
+			pHidden : true,
+		};
+
+	}
 
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+				<button 
+				id="click"
+				onClick={()=>{
+					this.setState({pHidden:false});
+				}}
+				>
+					Button
+					</button>
+				
+				{this.state.pHidden ? null : (
+				<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)}
     		</div>
     	);
     }
